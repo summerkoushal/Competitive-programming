42ded5fc86db029d8d4ab84bfd7a5b39bb05dd22
summekoushal <2020bcs003@sggs.ac.in>
2026-07-14 13:10:40 +0530

    updated

--- newBoilerplate.cpp
+++ newBoilerplate.cpp
@@ -1,34 +1,22 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
+// #define pb push_back
+// #define all(x) (x).begin(), (x).end()
+#define debug(x) cerr << (#x) << ":\t" << (x) << endl
+const ll MOD = 1e9+7;
 using namespace std;
 
-// #ifndef ONLINE_JUDGE
-// #define debug(x) cerr << #x << " " << x << endl;
-// #else 
-// #define debug(x)
-// #endif
-
-ll pow(ll a, ll x) { ll ans = 1; while(x--) ans *= a; return ans; }
+ll binexpMod(ll a, ll b) { ll x = 1; while (b > 0) { if (b & 1) x = (x * a) % MOD; a = (a * a) % MOD; b >>= 1; } return x; }
+ll binexp(ll a, ll b) { ll x = 1; while (b > 0) { if (b & 1) x = (x * a); a = (a * a); b >>= 1; } return x; }
+ll inverseMod(ll a, ll m) { a = a % m; for (ll x = 1; x < m; x++) if ((a * x) % m == 1) return x; return -1; }
+ll dividemod(ll x, ll y) { return ((x % MOD) * binexpMod(y % MOD, MOD - 2)) % MOD; }
+ll multiplymod(ll x, ll y) { return ((x % MOD) * (y % MOD)) % MOD; }
 
 void soln() {
-// #ifndef ONLINE_JUDGE
-//     freopen("Error.txt", "w", "stderr");
-// #endif
-    int n = 100;
-    // cin >> n;
-    // n *= 10;
-    // int b = 3 * n - 14;
-
-    // debug(n);
-    // debug(b);
-    cout << n << endl;
-    
+    // get some fast debugger
 }
 
 int main() {
-
     ll t;
     // cin >> t; while(t--) 
     soln();
