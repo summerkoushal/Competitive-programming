693244eacee8d26776f74ddb12c9b5b3691a5a55
summekoushal <2020bcs003@sggs.ac.in>
2026-07-14 11:26:30 +0530

    Apply .gitignore and remove ignored files from tracking

--- mistakes.cpp
+++
@@ -1,6 +0,0 @@
-// 1 : forgot that if buckets are remaining then we can plant the rows
-// 1 : queue
-// 1 : bfs
-// 1 : bracket sequence
-// 1 : number theory > prime numbers
-// 1 : number theory > modulo multiplicative inverse
